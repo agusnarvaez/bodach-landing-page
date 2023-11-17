ffdb85126b773649244c60204b9b471ec559080c
agusnarvaez <agus.narvaez@outlook.com>
2023-11-17 18:58:50 -0300

    Corrijo tsconfig y agrego app.module

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+//  Módulos angular
+import { NgModule } from '@angular/core'
+/* import { AppRoutingModule } from './app-routing.module' */
+
+// Componentes
+
+// Páginas
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    /* FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    RouterOutlet,
+    RouterModule,
+    HttpClientModule,
+    NgOptimizedImage */
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule {}
