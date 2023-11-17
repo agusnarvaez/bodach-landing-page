82b1435afa3b65616493ae73cb27e930d266c0da
agusnarvaez <agus.narvaez@outlook.com>
2023-11-17 20:37:21 -0300

    Limpio app.component y corrijo app.module

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,10 +21,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('bodach-landing-page')
   })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    fixture.detectChanges()
-    const compiled = fixture.nativeElement as HTMLElement
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bodach-landing-page')
-  })
 })
