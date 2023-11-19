f93f16f622402391fa12556c47b1c4fe72d04457
agusnarvaez <agus.narvaez@outlook.com>
2023-11-19 04:52:21 -0300

    Add home-development component

---
+++ src/app/sections/home/home-development/home-development.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-home-development',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home-development.component.html',
+  styleUrl: './home-development.component.css'
+})
+export class HomeDevelopmentComponent {
+
+}
