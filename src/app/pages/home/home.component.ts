c506742bb607964b6c8734f5f8d3db745d9b4296
agusnarvaez <agus.narvaez@outlook.com>
2023-11-19 05:09:04 -0300

    Add new sections to home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,24 @@ import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HomeBannerComponent } from '../../sections/home/home-banner/home-banner.component'
 import { HomeIntroductionComponent } from '../../sections/home/home-introduction/home-introduction.component'
+import { HomeProductsComponent } from '../../sections/home/home-products/home-products.component'
+import { HomeDevelopmentComponent } from '../../sections/home/home-development/home-development.component'
+import { HomeSellersComponent } from '../../sections/home/home-sellers/home-sellers.component'
+import { HomeFaqComponent } from '../../sections/home/home-faq/home-faq.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,HomeBannerComponent,HomeIntroductionComponent,HomeBannerComponent],
+  imports: [
+    CommonModule,
+    HomeBannerComponent,
+    HomeIntroductionComponent,
+    HomeBannerComponent,
+    HomeProductsComponent,
+    HomeDevelopmentComponent,
+    HomeSellersComponent,
+    HomeFaqComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
