56d2bc8da9873e32632c939db90a2f12023d839d
agusnarvaez <agus.narvaez@outlook.com>
2023-11-19 02:17:04 -0300

    Agrego banner e introduction a home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { HomeBannerComponent } from '../../sections/home/home-banner/home-banner.component'
+import { HomeIntroductionComponent } from '../../sections/home/home-introduction/home-introduction.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,HomeBannerComponent,HomeIntroductionComponent,HomeBannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
