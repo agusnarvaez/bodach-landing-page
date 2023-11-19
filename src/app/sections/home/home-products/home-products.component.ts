40fb4a6a95a39aec69b7a89d4a691d66fab33f4c
agusnarvaez <agus.narvaez@outlook.com>
2023-11-19 05:09:34 -0300

    Add products section to home page

--- src/app/sections/home/home-products/home-products.component.ts
+++ src/app/sections/home/home-products/home-products.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { ButtonComponent } from '../../../components/button/button.component'
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-home-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ButtonComponent,RouterModule],
   templateUrl: './home-products.component.html',
   styleUrl: './home-products.component.css'
 })
