d939456be03a16926b64c155941d15c4a11fad2b
agusnarvaez <agus.narvaez@outlook.com>
2023-11-19 04:52:14 -0300

    Add home-faq component files

---
+++ src/app/sections/home/home-faq/home-faq.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-home-faq',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home-faq.component.html',
+  styleUrl: './home-faq.component.css'
+})
+export class HomeFaqComponent {
+
+}
