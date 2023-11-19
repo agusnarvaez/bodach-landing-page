5ec6e662d7baba55664ff467b4c15b0d817d5008
agusnarvaez <agus.narvaez@outlook.com>
2023-11-19 04:51:57 -0300

    Add home-products component

---
+++ src/app/sections/home/home-products/home-products.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-home-products',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home-products.component.html',
+  styleUrl: './home-products.component.css'
+})
+export class HomeProductsComponent {
+
+}
