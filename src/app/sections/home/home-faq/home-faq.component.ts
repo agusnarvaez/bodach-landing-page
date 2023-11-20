7d7b794d9d09f92a8485c9fcbdd1c63401df0819
agusnarvaez <agus.narvaez@outlook.com>
2023-11-20 04:00:56 -0300

    Agrego sección faq

--- src/app/sections/home/home-faq/home-faq.component.ts
+++ src/app/sections/home/home-faq/home-faq.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { FaqItemComponent } from '../../../components/faq-item/faq-item.component'
 
 @Component({
   selector: 'app-home-faq',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FaqItemComponent],
   templateUrl: './home-faq.component.html',
   styleUrl: './home-faq.component.css'
 })
 export class HomeFaqComponent {
+  constructor() {}
+  faqList = [
+    {
+      question: 'What is the best way to get started?',
+      answer:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
+    },
+    {
+      question: 'What is the best way to get started?',
+      answer:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
+    },
+    {
+      question: 'What is the best way to get started?',
+      answer:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
+    },
+    {
+      question: 'What is the best way to get started?',
+      answer:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
+    },
+    {
+      question: 'What is the best way to get started?',
+      answer:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
+    },
+    {
+      question: 'What is the best way to get started?',
+      answer:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.'
+    }
+  ]
 
 }
