05bad257a74f2a744ae0802f77fb4b49d7e031cb
agusnarvaez <agus.narvaez@outlook.com>
2023-11-27 21:33:58 -0300

    Agrego estilo y binding de contact

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core'
+import { FormsModule, NgForm } from '@angular/forms'
+import { Component, ViewChild } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { InputComponent } from '../../components/input/input.component'
+import { ButtonComponent } from '../../components/button/button.component'
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,InputComponent,FormsModule,ButtonComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
 
+
+export class ContactComponent {
+  @ViewChild('myForm') myForm!: NgForm
+  message: string = ''
+  fullName: string = ''
+  email: string = ''
+  phone: string = ''
+  subject: string = ''
+
+  onSubmit() {
+    console.log(this.myForm.value)
+    /* this.message = 'Mensaje enviado' */
+    /* this.myForm.reset() */
+  }
 }
