2536bd6c4aab808a6da88f49ff8b99344dc7f38a
agusnarvaez <agus.narvaez@outlook.com>
2023-12-01 20:58:54 -0300

    Creo componente card

---
+++ src/app/sections/products/products-card/products-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-products-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './products-card.component.html',
+  styleUrl: './products-card.component.css'
+})
+export class ProductsCardComponent {
+
+}
